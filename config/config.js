047c81dc12c9cc319273b659db0339801b8f03a3
Vinod Liyanage <vinodsliyanage@gmail.com>
2022-02-23 00:47:13 +0530

    change the configaration.

--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 const config = {
-    windowSize : {
-        width: 500,
-        height: 500
-    },
-    windowPosition: {
-        x: -1000,
-        y: -1000
-    }
-}
+  windowSize: {
+    width: 500,
+    height: 500,
+  },
+  windowPosition: {
+    x: -1000,
+    y: -1000,
+  },
+};
 
 module.exports = config;
